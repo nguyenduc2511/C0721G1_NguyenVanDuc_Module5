610ffde284708b3bdb330cdd1aaff4a8809a13b0
nguyenduc2511 <nguyenduc251196@gmail.com>
2021-12-08 23:12:22 +0700

    bai7

---
+++ case/case-study/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateCustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
